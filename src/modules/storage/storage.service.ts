de1a67964562a8c3fcdd12d497301b165c85ad2b
Dyego Cruz <dyegocruz@gmail.com>
2023-08-13 17:27:20 -0300

    Refactoring storage

--- src/modules/storage/storage.service.ts
+++ src/modules/storage/storage.service.ts
@@ -1,11 +1,12 @@
 import { Upload } from "@aws-sdk/lib-storage";
 import { S3Client } from "@aws-sdk/client-s3";
+import getEnv from "../../config/env.config";
 
 export const uploadFile = (fileName: string, data: any) => {
   return new Upload({
     client: new S3Client(),
     params: {
-      Bucket: process.env.AWS_BUCKET_NAME,
+      Bucket: getEnv.getString("AWS_BUCKET_NAME"),
       Key: `${fileName}`,
       Body: JSON.stringify(data),
     },
